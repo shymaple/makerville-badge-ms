483cd357f913e99a7fe3a12a04086d5240168e4b
Anuj Deshpande <anuj@makerville.io>
2025-02-26 11:52:42 +0800

    Add button code (commented as buggy)
    Seeing i2c crashes when button enabled - not sure why yet

--- app/src/main.c
+++ app/src/main.c
@@ -4,11 +4,21 @@
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/display/cfb.h>
+#include <zephyr/drivers/gpio.h>
 
 LOG_MODULE_REGISTER(BADGE, LOG_LEVEL_DBG);
 const struct device *display_dev;
 
-int display_text()
+#define SW0_NODE	DT_ALIAS(sw0)
+#if !DT_NODE_HAS_STATUS_OKAY(SW0_NODE)
+#error "Unsupported board: sw0 devicetree alias is not defined"
+#endif
+
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios,
+                                                              {0});
+static struct gpio_callback button_cb_data;
+
+int display_init()
 {
   uint16_t x_res;
 	uint16_t y_res;
@@ -41,16 +51,17 @@ int display_text()
 	rows = cfb_get_display_parameter(display_dev, CFB_DISPLAY_ROWS);
 	ppt = cfb_get_display_parameter(display_dev, CFB_DISPLAY_PPT);
 
-	for (int idx = 0; idx < 42; idx++) {
-		if (cfb_get_font_size(display_dev, idx, &font_width, &font_height)) {
-			break;
-		}
-		cfb_framebuffer_set_font(display_dev, idx);
-		printf("font width %d, font height %d\n",
-		       font_width, font_height);
-    if (idx==2)
-      break;
-	}
+	/* for (int idx = 0; idx < 42; idx++) { */
+	/* 	if (cfb_get_font_size(display_dev, idx, &font_width, &font_height)) { */
+	/* 		break; */
+	/* 	} */
+	/* 	cfb_framebuffer_set_font(display_dev, idx); */
+	/* 	printf("font width %d, font height %d\n", */
+	/* 	       font_width, font_height); */
+  /*   if (idx==2) */
+  /*     break; */
+	/* } */
+  cfb_framebuffer_set_font(display_dev, 0);
 
 	printf("x_res %d, y_res %d, ppt %d, rows %d, cols %d\n",
 	       x_res,
@@ -63,35 +74,56 @@ int display_text()
 
 	cfb_set_kerning(display_dev, 0);
   cfb_print(display_dev,
-            "Anuj D",
+            "ANUJ        DESHPANDE",
             0, 0);
   cfb_framebuffer_finalize(display_dev);
-
-	/* while (1) { */
-	/* 	for (int i = 0; i < MIN(x_res, y_res); i++) { */
-	/* 		cfb_framebuffer_clear(display_dev, false); */
-	/* 		if (cfb_print(display_dev, */
-	/* 			      "Anuj D", */
-	/* 			      i, i)) { */
-	/* 			printf("Failed to print a string\n"); */
-	/* 			continue; */
-	/* 		} */
-
-	/* 		cfb_framebuffer_finalize(display_dev); */
-	/* 	} */
-	/* } */
 }
+
+int count = 0;
+void button_pressed(const struct device *dev, struct gpio_callback *cb,
+                    uint32_t pins)
+{
+	printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
+  /* char text[32]; */
+  /* sprintf(text,"Anuj %d",count++); */
+  /* display_text(text); */
+}
+
 
 int main(void)
 {
-
+  int ret;
   display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
   if (!device_is_ready(display_dev)) {
     printk("Display_Device %s not ready\n", display_dev->name);
-    return 0;
+    /* return 0; */
   }
 
   LOG_INF("SSD1306 device found\n");
-  display_text();
+  display_init();
+
+	if (!gpio_is_ready_dt(&button)) {
+		LOG_INF("Error: button device %s is not ready\n",
+		       button.port->name);
+	}
+  /* TODO Uncommenting everything below this causes issues in I2C */
+	/* ret = gpio_pin_configure_dt(&button, GPIO_INPUT); */
+	/* if (ret != 0) { */
+	/* 	LOG_INF("Error %d: failed to configure %s pin %d\n", */
+	/* 	       ret, button.port->name, button.pin); */
+	/* } */
+	/* ret = gpio_pin_interrupt_configure_dt(&button, */
+  /*                                       GPIO_INT_EDGE_TO_ACTIVE); */
+	/* if (ret != 0) { */
+	/* 	LOG_INF("Error %d: failed to configure interrupt on %s pin %d\n", */
+  /*          ret, button.port->name, button.pin); */
+	/* 	return 0; */
+	/* } */
+	/* gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin)); */
+	/* gpio_add_callback(button.port, &button_cb_data); */
+	/* LOG_INF("Set up button at %s pin %d\n", button.port->name, button.pin); */
+
+  /* k_msleep(1000); */
+
   return 0;
 }
